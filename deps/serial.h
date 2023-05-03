5f87190c726c596ef9574d8caae070182cb9825f
Danitim <danitim4@gmail.com>
2023-05-03 19:39:14 +0300

    Temporal fixes

---
+++ deps/serial.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <time.h>
+
+void set_tty_flags(struct termios* tty);
+
